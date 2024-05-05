5c8a2b2c2c95f2b76ce82d833c407467908208d9
Sukhpreet Singh <panesar.pb08@gmail.com>
2024-05-04 22:48:24 -0400

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,22 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, JsonPipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsyncPipe, JsonPipe],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
+    <h1>Example app</h1>
+    <pre><code>{{ res | async | json }}</code></pre>
     <router-outlet />
   `,
   styles: [],
 })
 export class AppComponent {
-  title = 'ngx-ssr-api';
+  private http = inject(HttpClient);
+
+  res = this.http.get('/api/hello');
 }
+
